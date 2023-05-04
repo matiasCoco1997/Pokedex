faf1026f4b88c5f80a1b5b3c0928d506c4d7df4a
Matias Ariel Coco <cocomatias6@gmail.com>
2023-05-04 14:10:24 -0300

    Pokemon commit

---
+++ pokemon.php
@@ -0,0 +1,90 @@
+
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="http://localhost/Pokedex/src/css/style.css">
+        <title>Pokedex</title>
+    </head>
+
+    <body>
+        <main>
+
+            <header>
+
+                <span class="logo"><img src="src/images/logo-pokebola.png"></span>
+
+                <h1>Pokédex</h1>
+
+                <form class="formLogin" method="POST" enctype="application/x-www-form-urlencoded" action="">
+
+                    <label>Login</label>
+
+                    <input name="user" placeholder="Usuario">
+
+                    <input name="password" placeholder="Contraseña">
+
+                    <button type="submit" name="submit">Ingresar</button>
+
+                </form>
+
+            </header>
+
+            <section>
+
+                <article class="contenedorPokemon">
+
+                    <span class="imgPokemon">
+                        <img src="src/images/charmander.png" alt="img pokemon">
+                    </span>
+
+                    <div>
+
+                        <span class="imgTipoPokemon">
+                            <img src="src/images/Tipo_fuego.png">
+                        </span>
+
+                        <h2>1 Charmander</h2>
+
+                        <p class="descripcionPokemon">
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                            DESCRIPCION DEL POKEMON
+                        </p>
+
+                    </div>
+
+                </article>
+            </section>
+        </main>
+    </body>
+</html>
