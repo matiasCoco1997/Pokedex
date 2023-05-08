73c8c37237e87d388a2f1ae14710f91576c8a20e
NuriaROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2023-05-07 22:45:06 -0300

    add update pokemon

--- components/updatePokemon.php
+++ components/updatePokemon.php
@@ -23,25 +23,71 @@ if (!$conexion) {
     die("La conexión falló: " . mysqli_connect_error());
 }
 
-$id = $_GET['id'];
+if (empty($_POST)) {
 
-$sql = "SELECT * FROM pokemones WHERE IDPokemon=$id";
+    $id = $_GET['id'];
 
-$resultado = $conexion->query($sql);
+    $sql = "SELECT * FROM pokemones WHERE IDPokemon=$id";
+
+    $resultado = $conexion->query($sql);
 
 
-if (mysqli_num_rows($resultado) == 1) {
-    $fila = mysqli_fetch_assoc($resultado);
-    $nombre = $fila['nombre'];
-    $numero = $fila['numero'];
-    $imagen = $fila['imagen'];
-    $tipo = $fila['tipo'];
-    $isEnabled = $fila['isEnabled'];
-    $descripcion = $fila['descripcion'];
+    if (mysqli_num_rows($resultado) == 1) {
+        $fila = mysqli_fetch_assoc($resultado);
+        $nombre = $fila['nombre'];
+        $numero = $fila['numero'];
+        $imagen = $fila['imagen'];
+        $tipo = $fila['tipo'];
+        $isEnabled = $fila['isEnabled'];
+        $descripcion = $fila['descripcion'];
+    }
+} else {
+    $id = $_POST['id'] ?? $_POST['id'];
+    $tipo = $_POST['opcion'] ?? $_POST['opcion'];
+    $numero = $_POST['img-numero-Pokemon'] ?? $_POST['img-numero-Pokemon'];
+    $nombre = $_POST['nombre-Pokemon'] ?? $_POST['nombre-Pokemon'];
+    $descripcion = $_POST['descripcion-Pokemon'] ?? $_POST['descripcion-Pokemon'];
+    $isEnabled = 1;
+
+    $directorio = "../../Pokedex/src/images/";
+
+    // Construir la consulta UPDATE
+    $sql = "UPDATE pokemones SET";
+
+    if ($_FILES["img-Pokemon"]["name"] == "") {
+        echo "No se recibió ningún archivo en la solicitud POST";
+    } else {
+        $imagen = $_FILES["img-Pokemon"];
+
+        $ruta_destino = $directorio . basename($_FILES["img-Pokemon"]["name"]);
+
+        $sql .= " imagen = '$ruta_destino',";
+    }
+
+    if (!empty($nombre)) {
+        $sql .= " nombre = '$nombre',";
+    }
+
+    if (!empty($numero)) {
+        $sql .= " numero = '$numero',";
+    }
+
+    if (!empty($tipo)) {
+        $sql .= " tipo = '$tipo',";
+    }
+
+    if (!empty($descripcion)) {
+        $sql .= " descripcion = '$descripcion',";
+    }
+
+    $sql = rtrim($sql, ",");
+
+    $sql .= " WHERE IDPokemon = $id";
+
+    $conexion->query($sql);
+
+    header("Location: ../index.php");
 }
-
-echo $nombre;
-
 
 ?>
 
@@ -51,46 +97,67 @@ echo $nombre;
 
         <article>
 
-            <form method='POST' enctype='multipart/form-data' action='newPokemon.php'>
+            <form method='POST' enctype='multipart/form-data' action='updatePokemon.php'>
 
                 <div>
                     <label for="img-Pokemon">Imagen del Pokemon:</label>
-                    <input type="file" placeholder="Cambiar imagen" name="img-Pokemon" value="<?php echo $imagen; ?>" required>
+                    <input type="file" placeholder="Cambiar imagen" name="img-Pokemon">
                     <?php if ($imagen) { ?>
                         <img class="visualizacion-imagen" src="<?php echo $imagen; ?>" alt="Imagen actual del Pokemon">
                     <?php } ?>
                 </div>
 
-
-
                 <div>
                     <label for="img-tipo-Pokemon">Seleccione el tipo de Pokemon:</label>
-                    <select id="opcion" name="opcion" required>
+                    <select id="opcion" name="opcion">
 
-                        <option value="../../Pokedex/src/images/Tipo_fuego.png" <?php if ($tipo == "../../Pokedex/src/images/Tipo_fuego.png") { echo " selected"; } ?>>Fuego</option>
-                        <option value="../../Pokedex/src/images/Tipo_planta.png" <?php if ($tipo == "../../Pokedex/src/images/Tipo_planta.png") { echo " selected"; } ?>>Planta</option>
-                        <option value="../../Pokedex/src/images/Tipo_agua.png" <?php if ($tipo == "../../Pokedex/src/images/Tipo_agua.png") { echo " selected"; } ?>>Agua</option>
-                        <option value="../../Pokedex/src/images/Tipo_electrico.png" <?php if ($tipo == "../../Pokedex/src/images/Tipo_electrico.png") { echo " selected"; } ?>>Eléctrico</option>
+                        <option
+                            value="../../Pokedex/src/images/Tipo_fuego.png" <?php if ($tipo == "../../Pokedex/src/images/Tipo_fuego.png") {
+                            echo " selected";
+                        } ?>>Fuego
+                        </option>
+                        <option
+                            value="../../Pokedex/src/images/Tipo_planta.png" <?php if ($tipo == "../../Pokedex/src/images/Tipo_planta.png") {
+                            echo " selected";
+                        } ?>>Planta
+                        </option>
+                        <option
+                            value="../../Pokedex/src/images/Tipo_agua.png" <?php if ($tipo == "../../Pokedex/src/images/Tipo_agua.png") {
+                            echo " selected";
+                        } ?>>Agua
+                        </option>
+                        <option
+                            value="../../Pokedex/src/images/Tipo_electrico.png" <?php if ($tipo == "../../Pokedex/src/images/Tipo_electrico.png") {
+                            echo " selected";
+                        } ?>>Eléctrico
+                        </option>
 
                     </select>
                 </div>
 
                 <div>
+                    <input type="hidden" name="id" value="<?php echo $id ?>">
+                </div>
+
+                <div>
                     <label for="img-numero-Pokemon">Ingrese el número de Pokemon:</label>
-                    <input type="number" placeholder="Ingrese el número del Pokemon" name="img-numero-Pokemon" value="<?php echo $numero; ?>" required>
+                    <input type="number" placeholder="Ingrese el número del Pokemon" name="img-numero-Pokemon"
+                           value="<?php echo $numero; ?>">
                 </div>
 
                 <div>
                     <label for="nombre-Pokemon">Ingrese nombre del Pokemon</label>
-                    <input type="text" placeholder="Ingrese el nombre del Pokemon" name="nombre-Pokemon" value="<?php echo $nombre; ?>" required>
+                    <input type="text" placeholder="Ingrese el nombre del Pokemon" name="nombre-Pokemon"
+                           value="<?php echo $nombre; ?>">
                 </div>
 
                 <div>
                     <label for="descripcion-Pokemon">Ingrese la descripción del Pokemon</label>
-                    <input type="text" placeholder="Ingrese la descripción del Pokemon" name="descripcion-Pokemon" value="<?php echo $descripcion; ?>" required>
+                    <input type="text" placeholder="Ingrese la descripción del Pokemon" name="descripcion-Pokemon"
+                           value="<?php echo $descripcion; ?>">
                 </div>
 
-                <button type="submit" name="newPokemon">Crear Pokemon</button>
+                <button type="submit" name="newPokemon">Actualizar datos</button>
 
             </form>
 
