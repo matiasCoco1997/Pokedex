b39cc9510a545cf5c0748587c6b1f6a5c997faeb
NuriaROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2023-05-09 19:20:55 -0300

    fix required

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ include("components/header.php");
         <article>
 
             <form method='GET' enctype='application/x-www-form-urlencoded' action='index.php'>
-                <input type="text" placeholder="Ingrese el nombre, tipo o número de Pokemon" name="itemABuscar" required>
+                <input type="text" placeholder="Ingrese el nombre, tipo o número de Pokemon" name="itemABuscar">
 
                 <button type="submit" name="busqueda">¿Quién es este Pokemon?</button>
             </form>
