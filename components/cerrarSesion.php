592f100e2a948fde4982ff52e9f52d4f624dc16e
Matias Ariel Coco <cocomatias6@gmail.com>
2023-05-09 14:51:19 -0300

    update cerrarSesion
    En vez de borrar el archivo de seguridad, lo que hago es borrar el contenido

--- components/cerrarSesion.php
+++ components/cerrarSesion.php
@@ -2,8 +2,10 @@
 session_start();
 if($_SESSION["nombreUsuario"]){
     setcookie("seguridad",0,time()-1000, '/');
-    unlink("seguridad.txt");
-    header("location:../index.php");
+    $fp = fopen("seguridad.txt", "w"); // abrir archivo en modo escritura
+    ftruncate($fp, 0); // truncar contenido del archivo a cero bytes
+    fclose($fp); // cerrar el archivo
+    header("location: ../index.php");
     }else{
             header("location:../index.php");
 }
